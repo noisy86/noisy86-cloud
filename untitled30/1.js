c519386874a5a43d414d6ed616eaec7781f5ec54
noisy86 <57344673+noisy86@users.noreply.github.com>
2020-05-29 23:51:33 +0200

    todo

---
+++ untitled30/1.js
@@ -0,0 +1,29 @@
+// <li>
+// <span class="handle ui-sortable-handle">
+//     <i class="fa fa-ellipsis-v"></i>
+//     <i class="fa fa-ellipsis-v"></i>
+//     </span>
+//     <input type="checkbox" value="" name="">
+//     <span class="text">Design a nice theme</span>
+// <small class="badge badge-danger"><i class="fa fa-clock-o"></i> 2 mins</small>
+// <div class="tools">
+//     <i class="fa fa-edit"></i>
+//     <i class="fa fa-trash-o"></i>
+//     </div>
+//     </li>
+
+    var first = document.getElementById("first");
+    var li = document.createElement("li");
+first.appendChild(li);
+    var a = document.createElement("span");
+    li.appendChild(a);
+a.classList.add("kk");
+
+        var i = document.createElement("i");
+        i.classList.add("ll");
+    a.appendChild(i);
+
+
+
+
+
